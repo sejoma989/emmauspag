580c1940fed6d5d6858eefd0a2c5bb0e4a72dc6e
jandrey <jandrey.serna@utp.edu.co>
2021-03-31 13:08:34 -0500

    arreglando el header y footer

--- functions.php
+++ functions.php
@@ -1,17 +1,32 @@
 <?php
 
+//cargar el nombre de la pagina en la pestaña del navegador
+
+function init_template(){
+
+  add_theme_support('Post_thumbnails');
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme','init_template');
 
 //echo get_stylesheet_uri();
+// registro de los style para los css de la pagina
 
- add_action('wp_enqueue_scripts', 'enqueue_styles');
+
  function enqueue_styles() {
- 	wp_register_style('theme_style', get_stylesheet_uri(), array(), time());
+ 	wp_register_style('theme_style', get_stylesheet_uri(), array(), time(), 'all');
  	wp_enqueue_style('theme_style');
   wp_register_style('theme_style_2', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', array(), time());
   wp_enqueue_style('theme_style_2');
+  wp_register_style('theme_style_3', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', array(), time());
+  wp_enqueue_style('theme_style_3');
+  wp_register_style('theme_style_4', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', array(), time());
+  wp_enqueue_style('theme_style_4');
+  wp_register_style('theme_style_5', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array(), time());
+  wp_enqueue_style('theme_style_5');
+  wp_register_style('theme_style_6', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', array(), time());
+  wp_enqueue_style('theme_style_6');
 
  }
- // wp_enqueue_scripts('script-1','https://code.jquery.com/jquery-3.2.1.slim.min.js');
- // wp_enqueue_scripts('script-2','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js');
- // wp_enqueue_scripts('script-3','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js');
- // wp_enqueue_scripts('script-4','https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js');
+add_action('wp_enqueue_scripts', 'enqueue_styles');
---
+++ header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset=utf-8>
+  <?php wp_head() ?>
+
+
+
+
+
+<?php wp_head(); ?>
+</head>
+<body>
+  <img src="http://localhost/tercero/wordpress/wp-content/uploads/2021/03/LOGOSOLO.png" class="logo">
